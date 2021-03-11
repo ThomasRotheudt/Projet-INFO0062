6d2de8deae2714e5c2d2cbc25cd8a87eb7f27114
Thomas Rotheudt <meneer@MBP-de-Thomas.lan>
2021-03-11 22:55:10 +0100

    Mise à jour

--- Code/MainGame/Game.java
+++ Code/MainGame/Game.java
@@ -1,7 +1,7 @@
-package Code;
+package Code.MainGame;
 
 public class Game {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ Code/Personnage/Personnage.java
@@ -0,0 +1,5 @@
+package Code.Personnage;
+
+public class Personnage {
+    
+}
